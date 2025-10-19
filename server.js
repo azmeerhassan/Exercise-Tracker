2e2162f90d1b675f3fffd6a3e00a17749ba4d0af
Azmeer Hassan Ammad <azmeerhassanammad@gmail.com>
2025-10-19 07:03:06 +0500

    Add Users

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const User = require('./models/user')
 
 const app = express();
 app.use(cors());
@@ -15,6 +16,23 @@ mongoose.connect('mongodb+srv://azmeerhassanammad_db_user:QNsB0ktVSxFuDJhJ@clust
 app.get('/', (req, res) => {
   res.send('Exercise Tracker Microservice 🏋️‍♂️ <br> MongoDB connection is live!');
 });
+
+app.post('/api/users', async(req, res)=>{
+    try {
+        const { username } = req.body
+
+        let existingUser = await User.findOne({ username })
+        if(existingUser) return res.json(existingUser)
+        
+        const newUser = new User({ username })
+        const savedUser = await newUser.save()
+        
+        res.json(savedUser)
+    } catch (error) {
+        console.error(error)
+        res.status(500).json({error: 'Server error'})
+    }
+})
 
 const PORT = 3000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
